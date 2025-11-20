7e78e9769463dad38fcb3da80540c3981c62c956
Nader Saoudy <nsaoudy@hotmail.com>
2025-11-20 10:00:40 +0300

    Add CountryDTO

--- HotelListing/IRepository/IUnitOfWork.cs
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -1,11 +1,14 @@
 ﻿using HotelListing.Data;
 using System;
+using System.Threading.Tasks;
 
 namespace HotelListing.IRepository
 {
-    public class IUnitOfWork : IDisposable
+    public interface IUnitOfWork : IDisposable
     {
         IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
 
         public void Dispose()
         {
---
+++ HotelListing/Models/CountryDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class CountryDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50,ErrorMessage ="Country Name is to log")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short Country Name is to log")]
+        public string ShortName { get; set; }
+    }
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country Name is to log")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short Country Name is to log")]
+        public string ShortName { get; set; }
+    }
+}
