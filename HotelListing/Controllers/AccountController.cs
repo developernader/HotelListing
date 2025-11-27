bf15981c083f9381c2689fdc50b983d8a07bf5db
Nader Saoudy <nsaoudy@hotmail.com>
2025-11-27 15:10:04 +0300

    Added AccountController

---
+++ HotelListing/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly SignInManager<ApiUser> _signInManager
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApiUser> userManager, SignInManager<ApiUser> signInManager, ILogger<AccountController> logger, IMapper mapper)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            this._logger = logger;
+            this._mapper = mapper;
+        }
+
+
+    }
+}
