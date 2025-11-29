b2dcbdc0bb027b465ca1021193598be31b557f3f
Nader Saoudy <nsaoudy@hotmail.com>
2025-11-29 13:10:55 +0300

    Added Register endpoint and UserDTO

--- HotelListing/Controllers/AccountController.cs
+++ HotelListing/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HotelListing.Data;
+using HotelListing.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,17 +17,81 @@ namespace HotelListing.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<ApiUser> _userManager;
-        private readonly SignInManager<ApiUser> _signInManager
         private readonly ILogger<AccountController> _logger;
         private readonly IMapper _mapper;
 
-        public AccountController(UserManager<ApiUser> userManager, SignInManager<ApiUser> signInManager, ILogger<AccountController> logger, IMapper mapper)
+        public AccountController(UserManager<ApiUser> userManager, ILogger<AccountController> logger, IMapper mapper)
         {
             this._userManager = userManager;
-            this._signInManager = signInManager;
             this._logger = logger;
             this._mapper = mapper;
         }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Regestration Attempt for {userDTO.Email}");
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(modelState: ModelState);
+            }
+
+            try
+            {
+                var user = _mapper.Map<ApiUser>(userDTO);
+                user.UserName = userDTO.Email;
+
+                var result = await _userManager.CreateAsync(user);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(modelState: ModelState);
+                }
+
+                //return Ok();
+                return Accepted();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(Register)}");
+                return Problem($"Something Went Wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+        //[HttpPost]
+        //[Route("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginUserDTO loginUserDTO)
+        //{
+        //    _logger.LogInformation($"Login Attempt for {loginUserDTO.Email}");
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(modelState: ModelState);
+        //    }
+
+        //    try
+        //    {
+        //        var result = await _signInManager.PasswordSignInAsync(loginUserDTO.Email, loginUserDTO.Password, false, false);
+        //        if (!result.Succeeded)
+        //        {
+        //            return Unauthorized(loginUserDTO);
+        //        }
+
+        //        return Accepted();
+        //        //return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, $"Something Went Wrong in the {nameof(Login)}");
+        //        return Problem($"Something Went Wrong in the {nameof(Login)}", statusCode: 500);
+        //    }
+        //}
 
 
     }
