3477d01d8ad4c3e9c7409eb5c93486b7517bf336
Nader Saoudy <nsaoudy@hotmail.com>
2025-11-29 18:18:44 +0300

    Rename wrong column LasName to LastName

--- HotelListing/Data/ApiUser.cs
+++ HotelListing/Data/ApiUser.cs
@@ -9,7 +9,7 @@ namespace HotelListing.Data
     public class ApiUser : IdentityUser
     {
         public string FirstName { get; set; }
-        public string LasName { get; set; }
+        public string LastName { get; set; }
 
     }
 }
---
+++ HotelListing/Migrations/20251129151518_rename.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class rename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LasName",
+                table: "AspNetUsers",
+                newName: "LastName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "LasName");
+        }
+    }
+}
